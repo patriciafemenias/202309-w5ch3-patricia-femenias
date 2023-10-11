2ea4977ffde1005137a4efd1b8761395f24de75e
Patricia <patricia.femenias@gmail.com>
2023-10-11 22:45:07 +0200

    Add subclass King

---
+++ public/app/King/King.js
@@ -0,0 +1,11 @@
+import Character from "../Character/Character.js";
+
+class King {
+  yearsOfReign;
+
+  comunicate() {
+    return `${super.comunicate} You all are gonna die!`;
+  }
+}
+
+export default King;
